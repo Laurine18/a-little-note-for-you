0ec3411306bd7b8e62d34d9d541b0683b11effc3
Laurine18 <laurine_bl@outlook.com>
2025-04-02 18:10:41 +0200

    Create firebase.js

---
+++ pages/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
+
+const db = firebase.firestore();
+
+export default db;
