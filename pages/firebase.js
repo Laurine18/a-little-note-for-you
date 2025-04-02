a0109966dd94421fe7e48efe1a1ac52ccd6e13cc
Laurine18 <laurine_bl@outlook.com>
2025-04-02 18:40:10 +0200

    Update firebase.js

--- pages/firebase.js
+++ pages/firebase.js
@@ -1,21 +1,18 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAfoCpLf95gtV2Uv1TXQkzBdpJaVtttpao",
+  authDomain: "a-little-note-for-you.firebaseapp.com",
+  databaseURL: "https://a-little-note-for-you-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "a-little-note-for-you",
+  storageBucket: "a-little-note-for-you.firebasestorage.app",
+  messagingSenderId: "654538900689",
+  appId: "1:654538900689:web:4c317a3071f0709b7931f0",
+  measurementId: "G-P37X64H8M0"
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app();
-}
-
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 export default db;
